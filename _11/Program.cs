0342b072674a9416223adeb75d960f26eb794d11
Kirill Miller <belogorski2000@gmail.com>
2022-08-27 12:13:16 +0300

    Добавьте файлы проекта.

---
+++ _11/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение k");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение b");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Quadratic quadratic = new Quadratic(k, b);
+            Console.WriteLine(quadratic.Root());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ _11/Quadratic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    struct Quadratic
+    {
+        double k;
+        double b;
+
+        public Quadratic(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+        public string Root()
+        {
+            double x = -b / k;
+            if (k == 0)
+                return "Ошибка.";
+            else
+                return $"x = {x}";
+        }
+    }
+}
